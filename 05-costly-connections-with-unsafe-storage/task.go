50870821289a1bac7468aac4802aa9b53282e99b
Abhinaenae <abhinay.lavu@gmail.com>
2025-04-23 17:00:20 -0500

    problem 05 complete without optimizations

--- 05-costly-connections-with-unsafe-storage/task.go
+++ 05-costly-connections-with-unsafe-storage/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"log/slog"
+	"sync"
+)
+
 type Connection interface {
 	// Need call Connect before Send
 	// Take time to connect
@@ -28,4 +33,36 @@ type Saver interface {
 // Responses must be saved using Saver.Save.
 // Be careful: Saver.Save is not safe for concurrent use.
 func SendAndSave(creator ConnectionCreator, saver Saver, requests []string, maxConn int) {
+
+	saveCh := make(chan string, len(requests))
+	var wg sync.WaitGroup
+
+	for _, req := range requests {
+		conn, err := creator.NewConnection()
+		if err != nil {
+			slog.Error("Failed to create connection", "error", err)
+			continue
+		}
+		defer conn.Disconnect()
+		conn.Connect()
+
+		wg.Add(1)
+		go func(r string) {
+			defer wg.Done()
+			resp, err := conn.Send(r)
+			if err != nil {
+				slog.Error("Failed to send request", "error", err)
+				return
+			}
+			saveCh <- resp
+		}(req)
+	}
+
+	wg.Wait()
+	close(saveCh)
+
+	for resp := range saveCh {
+		saver.Save(resp)
+	}
+
 }

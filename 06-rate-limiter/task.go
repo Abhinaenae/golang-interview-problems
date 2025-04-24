cde3b4e6c7f6c646f30ee2ce3da034c38c1d4178
Abhinaenae <abhinay.lavu@gmail.com>
2025-04-24 16:03:18 -0500

    Problem 06 complete

--- 06-rate-limiter/task.go
+++ 06-rate-limiter/task.go
@@ -1,14 +1,28 @@
 package main
 
-type RateLimiter struct{}
+import "time"
+
+type RateLimiter struct {
+	ticker *time.Ticker
+}
 
 func NewRateLimiter(n int) *RateLimiter {
-	return &RateLimiter{}
+	opsPerSecond := time.Duration(n)
+	limit := time.Second / opsPerSecond
+	return &RateLimiter{
+		ticker: time.NewTicker(limit),
+	}
 }
 
 func (r *RateLimiter) CanTake() bool {
-	return false
+	select {
+	case <-r.ticker.C:
+		return true
+	default:
+		return false
+	}
 }
 
 func (r *RateLimiter) Take() {
+	<-r.ticker.C
 }

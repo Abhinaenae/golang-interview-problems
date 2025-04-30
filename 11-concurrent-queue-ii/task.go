0b65647b7ece3270112ac5bc9f12cd3c0070a9a9
Abhinaenae <abhinay.lavu@gmail.com>
2025-04-30 10:49:13 -0500

    Problem 11 complete

--- 11-concurrent-queue-ii/task.go
+++ 11-concurrent-queue-ii/task.go
@@ -1,25 +1,63 @@
+// This queue implementation uses a mutex-based approach using a slice instead of
+// managing increased complexity to accomodate the new peek() function that reads values
+// without removing them from the store
+
 package main
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrQueueFull = errors.New("queue is full")
 
-type Queue struct{}
+type Queue struct {
+	store   []int
+	maxSize int
+	mu      sync.Mutex
+}
 
 func NewQueue(size int) *Queue {
-	return &Queue{}
+	return &Queue{
+		store:   make([]int, 0, size),
+		maxSize: size,
+		mu:      sync.Mutex{},
+	}
 }
 
 func (q *Queue) Push(val int) error {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if len(q.store) >= q.maxSize {
+		return ErrQueueFull
+	}
+
+	q.store = append(q.store, val)
 	return nil
 }
 
 func (q *Queue) Pop() int {
-	return -1
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if len(q.store) == 0 {
+		return -1
+	}
+
+	poppedVal := q.store[0]
+	q.store = q.store[1:]
+	return poppedVal
 }
 
 func (q *Queue) Peek() int {
-	return -1
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if len(q.store) == 0 {
+		return -1
+	}
+
+	peekVal := q.store[0]
+	return peekVal
 }

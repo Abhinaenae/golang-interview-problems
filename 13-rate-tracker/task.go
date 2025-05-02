d8be3b890994a1e85896d5b1f1b8e752789f537a
Abhinaenae <abhinay.lavu@gmail.com>
2025-05-02 12:14:23 -0500

    Problem 13 complete

--- 13-rate-tracker/task.go
+++ 13-rate-tracker/task.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync/atomic"
 	"time"
 )
 
@@ -8,12 +9,29 @@ type Monitor interface {
 	SendRate(int)
 }
 
-type Handler struct{}
+type Handler struct {
+	count atomic.Uint64
+}
 
 func (h *Handler) Handle() {
 	// Some work
 	// Calculate rate of this function
+	h.count.Add(1)
+
 }
 
+// LogRate starts a goroutine that periodically reports the number of handled events
+// to the provided monitor. It samples the count every duration d and resets the counter
+// after each report. The monitoring continues until the program terminates.
 func (h *Handler) LogRate(monitor Monitor, d time.Duration) {
+	go func() {
+		ticker := time.NewTicker(d)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			currCount := h.count.Swap(0)
+			monitor.SendRate(int(currCount))
+
+		}
+	}()
 }
